67e801cd1f2659384b3ba743af8d6673ca8ee0f6
unknown <lksh@PLLL0065.gft.com>
2016-07-21 15:09:56 +0200

    Initial

---
+++ scan.js
@@ -0,0 +1,47 @@
+var request = require('request');
+
+var ONE_KM_IN_DEG_LAT = 1 / 110.574;
+var SPACING_KM = 0.5;
+var RANGE_KM = 5;
+
+function getOneKmInDegLong(lat) {
+    return 1 / (111.320 * Math.cos(lat * Math.PI / 180));
+}
+
+function getScanPoints(currentPosition, range, spacing) {
+    range = range || RANGE_KM;
+    spacing = spacing || SPACING_KM;
+
+    var long = Number(currentPosition.split(',')[1].trim()),
+        lat = Number(currentPosition.split(',')[0].trim()),
+        downBound = lat - range * ONE_KM_IN_DEG_LAT,
+        upBound = lat + range * ONE_KM_IN_DEG_LAT,
+        leftBound = long - range * getOneKmInDegLong(lat),
+        rightBound = long + range * getOneKmInDegLong(lat),
+        scanPoints = [],
+        latPointer = downBound,
+        longPointer = leftBound;
+
+    while (longPointer < rightBound) {
+        while (latPointer < upBound) {
+            scanPoints.push(latPointer + '/' + longPointer);
+            latPointer += spacing * ONE_KM_IN_DEG_LAT;
+
+        }
+        latPointer = downBound;
+        longPointer += spacing * getOneKmInDegLong(latPointer);
+    }
+
+    return scanPoints;
+}
+
+function scan(position, range, spacing) {
+    var scanPoints = getScanPoints(position, range, spacing);
+
+    console.log('Requesting ' + scanPoints.length + ' points');
+    scanPoints.forEach(function (scanPoint) {
+        request.get('https://pokevision.com/map/scan/' + scanPoint);
+    });
+}
+
+module.exports = scan;
